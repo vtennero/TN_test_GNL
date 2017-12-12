4a4fe5a250e0a6dd1e7542bcbc678faf027a2103
Victor TENNERONI <vtennero@student.42.fr>
2017-12-12 16:42:05 +0100

    Tracer Tong

--- resources/norme.sh
+++ resources/norme.sh
@@ -5,12 +5,12 @@ norm()
 	norminette get_next_line.c get_next_line.h libft/ > norm.txt
 	local currentErrorLine
 	local previousErrorLine=-100
-	local TOTAL_ERR=$(cat norm.txt | grep Error | wc -l)
+	local TOTAL_ERR=$(cat norm.txt | grep 'Warning\|Error' | grep -v 'Not a valid file' | wc -l)
 	local i=1
 
 	while [ $i -le "$TOTAL_ERR" ]
 	do
-		currentErrorLine=$(cat -n norm.txt | grep Error | head -n $i | cut -d $'\t' -f1)
+		currentErrorLine=$(cat -n norm.txt | grep 'Warning\|Error' | grep -v 'Not a valid file' | head -n $i | cut -d $'\t' -f1)
 		currentErrorLine=$(echo $currentErrorLine | cut -d " " -f$i)
 		if [ $(($previousErrorLine + 1)) != $currentErrorLine ]
 		then
@@ -22,3 +22,4 @@ norm()
 	done
 	rm norm.txt
 }
+norm
