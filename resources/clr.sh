95233f1664f50cabb8bbf1f972ab81a42c712b57
Victor TENNERONI <vtennero@student.42.fr>
2018-02-14 11:45:49 +0100

    Tracer Tong

--- resources/clr.sh
+++ resources/clr.sh
@@ -2,10 +2,10 @@
 
 clr()
 {
-	rm norminette_log.txt
-	rm test_gnl1 test_gnl2 test_gnl3 test_gnl4 test_gnl5
-	rm -rf test_gnl1.dSYM test_gnl2.dSYM test_gnl3.dSYM test_gnl4.dSYM test_gnl5.dSYM
-	rm -rf tests_output_test_gnl1 tests_output_test_gnl2 tests_output_test_gnl3 tests_output_test_gnl4 tests_output_test_gnl5
-	rm -rf valgrind_logs
-	rm -rf oui_qui_leaks_logs
+	rm norminette_log.txt > /dev/null 2>&1
+	rm test_gnl1 test_gnl2 test_gnl3 test_gnl4 test_gnl5 > /dev/null 2>&1
+	rm -rf test_gnl1.dSYM test_gnl2.dSYM test_gnl3.dSYM test_gnl4.dSYM test_gnl5.dSYM > /dev/null 2>&1
+	rm -rf tests_output_test_gnl1 tests_output_test_gnl2 tests_output_test_gnl3 tests_output_test_gnl4 tests_output_test_gnl5 > /dev/null 2>&1
+	rm -rf valgrind_logs > /dev/null 2>&1
+	rm -rf oui_qui_leaks_logs > /dev/null 2>&1
 }
